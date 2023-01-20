8035abb0ed6f0132f32a31647b25dfa3bc47cb9f
thanatadcs <thanatad.cs@gmail.com>
2023-01-21 00:38:05 +0700

    Added get and create product api

--- vending_machine/models.py
+++ vending_machine/models.py
@@ -1,9 +1,20 @@
+from typing import List
 from . import db
 from dataclasses import dataclass
 
+@dataclass
+class Product(db.Model):
+    __tablename__ = 'product'
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name: str = db.Column(db.String(100))
+    price: float = db.Column(db.Numeric(precision=5, scale=2)) # price cannot exceed 999.99
+    quantity: int = db.Column(db.Integer)
+    vending_machine_id: int = db.Column(db.Integer, db.ForeignKey('vending_machine.id'), nullable=False)
+    
 @dataclass
 class VendingMachine(db.Model):
     __tablename__ = 'vending_machine'
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name: str = db.Column(db.String(100))
     location: str = db.Column(db.Text)
+    products: List[Product] = db.relationship('Product', backref='vending_machine', cascade='all, delete-orphan')
--- vending_machine/routes.py
+++ vending_machine/routes.py
@@ -1,12 +1,13 @@
 from flask import current_app as app
 from flask import (request, jsonify)
 from flask.helpers import make_response
-from .models import (VendingMachine, db)
+from .models import (VendingMachine, Product, db)
 
+# Vending machine
 @app.route('/vending_machine')
 def list_vending_machine():
     results = VendingMachine.query.all()
-    return jsonify({"vending_machines":results})
+    return jsonify({"vending_machine":results})
 
 @app.route('/vending_machine', methods=['POST'])
 def create_vending_machine():
@@ -42,5 +43,24 @@ def delete_vending_machine():
     id = data.get('id')
     vending_machine = VendingMachine.query.filter_by(id=id).first()
     db.session.delete(vending_machine)
+    db.session.commit()
+    return jsonify({"status": "OK"})
+
+# Product
+@app.route('/product')
+def list_product():
+    results = Product.query.all()
+    return jsonify({"product":results})
+
+@app.route('/product', methods=['POST'])
+def create_product():
+    data: dict = request.get_json()
+    required_fields = set(['name', 'price', 'quantity', 'vending_machine_id'])
+    if not data or not (required_fields <= data.keys()):
+        return make_response(jsonify({'status': 'Bad Request'}), 400)
+    name, price, quantity, vm_id = \
+        data.get('name'), data.get('price'), data.get('quantity'), data.get('vending_machine_id')
+    new_product = Product(name=name, price=price, quantity=quantity, vending_machine_id=vm_id)
+    db.session.add(new_product)
     db.session.commit()
     return jsonify({"status": "OK"})
